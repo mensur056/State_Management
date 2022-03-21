1bf5776df2a1ce413bb3f6ac324cb3539e920202
mensur056 <mansur.sarxanov@gmail.com>
2022-03-21 15:38:58 +0100

    State Management

---
+++ lib/feature/onboard/on_board_models.dart
@@ -0,0 +1,9 @@
+class OnBoardModel {
+  final String title;
+  final String description;
+  final String imageName;
+
+  OnBoardModel(this.title, this.description, this.imageName);
+
+  String get imageWithPath => 'assets/images/$imageName.png';
+}
