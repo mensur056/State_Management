33a7b97a40e10f42007a6c7946526de94ff01c54
mensur056 <mansur.sarxanov@gmail.com>
2022-04-03 09:50:04 +0400

    'test'

--- lib/feature/login/view/login_view.dart
+++ lib/feature/login/view/login_view.dart
@@ -3,6 +3,8 @@ import 'package:state_management/product/constants/image_enum.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
+  final String buttonText = 'Login';
+  final String checkBoxTitle = 'Remember';
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +13,25 @@ class LoginView extends StatelessWidget {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only( left: 90, right: 90),
+            padding: const EdgeInsets.only(left: 90, right: 90),
             child: ImageEnums.door.toImage,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(buttonText),
+          ),
+          CheckboxListTile(
+            title:Text(checkBoxTitle),
+            value: true,
+            onChanged: (value) {},
           )
         ],
       ),
