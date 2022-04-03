9ce570da2a835a55c2e2e7c1d9b9be5b115a3ef3
mensur056 <mansur.sarxanov@gmail.com>
2022-04-03 09:57:40 +0400

    'test'

--- lib/feature/login/view/login_view.dart
+++ lib/feature/login/view/login_view.dart
@@ -10,34 +10,36 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 90, right: 90),
-            child: ImageEnums.door.toImage,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                const TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                ), ElevatedButton(
-                  onPressed: () {},
-                  child: Center(child: Text(buttonText)),
-                ),
-                CheckboxListTile(
-                  title:Text(checkBoxTitle),
-                  value: true,
-                  onChanged: (value) {},
-                )
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 90, right: 90),
+              child: ImageEnums.door.toImage,
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                  ), ElevatedButton(
+                    onPressed: () {},
+                    child: Center(child: Text(buttonText)),
+                  ),
+                  CheckboxListTile(
+                    title:Text(checkBoxTitle),
+                    value: true,
+                    onChanged: (value) {},
+                  )
+                ],
+              ),
+            ),
 
-        ],
+          ],
+        ),
       ),
     );
   }
